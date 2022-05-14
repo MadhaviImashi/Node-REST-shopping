f1018f69ef8252325a43f631bb1295a16e3201f3
MadhaviImashi <madhaviimashi@gmail.com>
2022-05-14 18:42:58 +0530

    product CRUD APIs created

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,30 +1,51 @@
 const express = require('express');
+const { default: mongoose } = require('mongoose');
 const router = express.Router();
+const Product = require('../models/product');
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
+    console.log('inside get products');
+    const all = await Product.find()
     res.status(200).json({
-        message: 'Handling GET request'
+        message: 'Handling GET request',
+        products: all
     })
 })
 
 router.post('/', (req, res, next) => {
-    res.status(201).json({
-        message: 'Handling POST request'
+    const product = new Product({
+        name: req.body.name,
+        price: req.body.price
+    });
+
+    product.save()
+        .then((result) => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Handling POST request',
+                newlyAddedProduct: product
+            })
+        })
+        .catch((err) => {
+            console.log(err.message);
+            res.status(500).json({
+                error: err.message
+            });
     })
 })
 
-router.get('./: prodID', (req, res, next) => {
+router.get('/: prodID', async (req, res, next) => {
     const id = req.params.prodID;
-    if (id === 'special') {
-        res.status(200).json({
-            message: ' YO discovered the special id',
-            id: id
-       })
-    } else {
-        res.status(200).json({
-            message: 'yo passed an id'
-        })
-    }
+    const doc = await Product.findById(id)
+        console.log('from db: ', doc);
+        // if (doc) {
+        //     res.status(200).json({doc})
+        // } else {
+        //     res.status(404).json({
+        //         message: 'No valid entry from for this product id'
+        //     })
+        // }
+    res.send(doc);
 })
 
 router.put('/:prodID', (req, res, next) => {
